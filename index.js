6eedb8388295d6effe85aaf2af82b4cafde26362
weijun1923 <asd101250101@gmail.com>
2023-02-15 17:16:38 +0800

    update cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const authRoute = require("./routes").auth;
+const cors = require("cors");
 
 // mongodb atlas connect
 mongoose.connect(process.env.DB_CONNECT, {
@@ -19,6 +20,7 @@ mongoose.connect(process.env.DB_CONNECT, {
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));//body parser
+app.use(cors());
 // loing and register middleware
 app.use("/api/user" , authRoute);
 
